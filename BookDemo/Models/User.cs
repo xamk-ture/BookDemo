6b826826ce9063ab4f4adb6f4c63a48b41436ba5
Tuomas Reijonen <hture01@xamk.fi>
2024-03-07 13:37:41 +0200

    Added enum

--- BookDemo/Models/User.cs
+++ BookDemo/Models/User.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace BookDemo.Models
 {
+    public enum Status
+    {
+        Active,
+        Inactive,
+        Supspended
+    }
+
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public Status Status { get; set; }
+
     }
 }
